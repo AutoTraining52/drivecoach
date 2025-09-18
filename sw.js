4377c6a82bf0cfbf5acc61ba9e0fb5f2e959fe74
autotraining <artemkim126@gmail.com>
2025-09-18 15:48:47 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'drivecoach-v1';
+const ASSETS = [
+  '/drivecoach/',
+  '/drivecoach/index.html',
+  '/drivecoach/manifest.webmanifest',
+  '/drivecoach/icon-192.png',
+  '/drivecoach/icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  const url = new URL(e.request.url);
+  if (url.pathname.startsWith('/drivecoach/')) {
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  }
+});
